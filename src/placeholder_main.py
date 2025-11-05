c9e4b0fa3024eb7ab5e8f2acb03345d634c5f1a5
Henry Bloch <63680706+gywn9081@users.noreply.github.com>
2025-11-04 19:44:08 -0600

    Simple main function

--- src/placeholder_main.py
+++ src/placeholder_main.py
@@ -1,1 +1,16 @@
 # This file will be the one that runs all of the outputs from antlr4
+import sys
+from antlr4 import *
+from build.src.ExprLexer import ExprLexer
+from build.src.ExprParser import ExprParser
+
+def main(input_stream):
+    lexer = ExprLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = ExprParser(stream)
+    tree = parser.prog()
+    print(tree.toStringTree(recog=parser))
+
+if __name__ == "__main__":
+    input_text = InputStream("3 + 4 * (2 - 1)\n")
+    main(input_text)
